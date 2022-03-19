3e7f8ddafd452bba83c51e4fb3a84053e745aa31
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-03-19 09:57:02 +0700

    run project

---
+++ app/src/main/java/huutoan/yomusic/Activity/MainActivity.java
@@ -0,0 +1,14 @@
+package huutoan.yomusic.Activity;
+
+import huutoan.yomusic.R;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/huutoan/yomusic/Fragment/Fragment_Home_Page.java
@@ -0,0 +1,21 @@
+package huutoan.yomusic.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import huutoan.yomusic.R;
+
+public class Fragment_Home_Page extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        return  view;
+    }
+}
