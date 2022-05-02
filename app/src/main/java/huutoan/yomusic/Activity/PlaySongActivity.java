ea0525a77a3f1af48e026f279261256c8de89454
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-05-02 19:32:04 +0700

    create UI play song

--- app/src/main/java/huutoan/yomusic/Activity/PlaySongActivity.java
+++ app/src/main/java/huutoan/yomusic/Activity/PlaySongActivity.java
@@ -1,10 +1,17 @@
 package huutoan.yomusic.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,12 +22,18 @@ import huutoan.yomusic.R;
 
 public class PlaySongActivity extends AppCompatActivity {
 
-
+    Toolbar toolbarPlaySong;
+    TextView textTimeSong, textTimeTotalSong;
+    SeekBar seekBarTimeSong;
+    ImageButton imgPlay, imgShuffle, imgNext, imgPrevious;
+    ViewPager viewPagerPlaySong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
+
+        initLayout();
 
         Intent intent = getIntent();
         if(intent.hasExtra("getSong")) {
@@ -37,7 +50,25 @@ public class PlaySongActivity extends AppCompatActivity {
         } else  {
             return;
         }
+    }
 
+    private void initLayout() {
+        toolbarPlaySong = findViewById(R.id.toolBarPlaySong);
+        textTimeSong = findViewById(R.id.textViewRunTime);
+        textTimeTotalSong = findViewById(R.id.textViewTimeTotal);
+        seekBarTimeSong = findViewById(R.id.seekBarTime);
+        imgPlay = findViewById(R.id.imageButtonPlayOfPause);
+        imgNext = findViewById(R.id.imageButtonNext);
+        imgPrevious = findViewById(R.id.imageButtonPrevious);
+        imgShuffle = findViewById(R.id.imageButtonPrevious);
+        viewPagerPlaySong = findViewById(R.id.viewPagerDiskSong);
+
+        setSupportActionBar(toolbarPlaySong);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarPlaySong.setNavigationOnClickListener((View view) -> {
+            finish();
+        });
+        toolbarPlaySong.setTitleTextColor(Color.WHITE);
 
     }
 }
